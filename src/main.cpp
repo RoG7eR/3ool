f15dd3c0b3aca5b1acc206bc5198181d678bdd29
RoG7eR <174163321+RoG7eR@users.noreply.github.com>
2025-08-10 06:41:07 +0530

    Just started with the project, setup cmake and asio is done, will work on portscanner now

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <asio/asio.hpp>
+
+int main() {
+    int choice;
+
+    std::cout << "===============" << std::endl;
+    std::cout << "=====3ool=====" << std::endl;
+    std::cout << "1. Portscanner" << std::endl;
+    std::cout << "2. Keylogger" << std::endl;
+    std::cout << "Enter your choice: " << std::endl;
+    std::cin >> choice;
+
+    
+}
